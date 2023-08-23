6aa2c5e3b5d53acb2d725cc2e898d584f735e352
bishalbhowmik <77568117+bishalbhowmik@users.noreply.github.com>
2023-08-23 18:42:41 +0600

    added Navbar and products

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Products from '../Products/Products';
 
 const Home = () => {
     return (
         <div>
             <div className="text-3xl">Home</div>
+            <Products></Products>
         </div>
     );
 };
---
+++ src/Pages/Product/Product.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Product.css';
+
+const Product = ({ product }) => {
+    const { name, img, seller, price, ratings } = product;
+    console.log(product);
+    return (
+
+
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={img} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{name}</h2>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
