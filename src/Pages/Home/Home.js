58ca0c56b450adc60fd267ba12c0a22f8fe22226
bishalbhowmik <77568117+bishalbhowmik@users.noreply.github.com>
2023-08-23 11:20:57 +0600

    initial setup done

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div className="text-3xl">Home</div>
+        </div>
+    );
+};
+
+export default Home;
